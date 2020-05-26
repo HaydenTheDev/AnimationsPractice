7f20109ebb13361b9178baba4ccc18665b54c743
Hayden Bazor <hbazor@murraystate.edu>
2020-05-26 08:18:17 -0500

    Commit includes the addition of a TextView and Onclick for the future animation.

--- app/src/main/java/hayden/project/animationspractice/MainActivity.java
+++ app/src/main/java/hayden/project/animationspractice/MainActivity.java
@@ -3,12 +3,27 @@ package hayden.project.animationspractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView txtHelloWorld;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Initialize
+        txtHelloWorld = findViewById(R.id.txtHelloWorld);
+
+        //Create onClick for the text
+        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
