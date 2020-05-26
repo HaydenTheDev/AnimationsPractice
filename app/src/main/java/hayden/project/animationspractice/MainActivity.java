962ecb2fd6d8ed4fe538fec302bfb803ca0f5461
Hayden Bazor <hbazor@murraystate.edu>
2020-05-26 10:00:55 -0500

    Changed the textView to ImageViews and added a fading tiger and lion.

--- app/src/main/java/hayden/project/animationspractice/MainActivity.java
+++ app/src/main/java/hayden/project/animationspractice/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -12,6 +13,8 @@ public class MainActivity extends AppCompatActivity {
 
     private TextView txtHelloWorld;
     private TextView txtHiWorld;
+    private ImageView lion;
+    private ImageView tiger;
 
     private boolean isHelloWorldShowing;
     @Override
@@ -20,11 +23,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //Initialize
-        txtHelloWorld = findViewById(R.id.txtHelloWorld);
-        txtHiWorld = findViewById(R.id.txtHiWorld);
+        lion = findViewById(R.id.imageViewLion);
+        tiger = findViewById(R.id.imageViewTiger);
+//        txtHelloWorld = findViewById(R.id.txtHelloWorld);
+//        txtHiWorld = findViewById(R.id.txtHiWorld);
 
         //Create onClick for the text
-        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+//        txtHelloWorld.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                //Log for button
+//                Log.i("MyTag", "This text was clicked.");
+//                if(isHelloWorldShowing){
+//                    animateHello();
+//                    isHelloWorldShowing = false;
+//                }else if(!isHelloWorldShowing) {
+//                    animateHi();
+//                    isHelloWorldShowing = true;
+//                }
+//
+//            }
+//        });
+        tiger.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 //Log for button
@@ -47,13 +67,13 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void animateHello(){
-        txtHelloWorld.animate().alpha(0f).setDuration(3000);
-        txtHiWorld.animate().alpha(1).setDuration(4000);
+        tiger.animate().alpha(0f).setDuration(3000);
+        lion.animate().alpha(1).setDuration(4000);
     }
 
     public void animateHi(){
-        txtHiWorld.animate().alpha(0f).setDuration(3000);
-        txtHelloWorld.animate().alpha(1).setDuration(4000);
+        lion.animate().alpha(0f).setDuration(3000);
+        tiger.animate().alpha(1).setDuration(4000);
 
     }
 
